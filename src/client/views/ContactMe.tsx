35afab300c791e36a32632104fccc72c7cbd5c31
MD <mdeanwill@gmail.com>
2024-04-04 11:22:23 -0700

    added mailgun integration

--- src/client/views/ContactMe.tsx
+++ src/client/views/ContactMe.tsx
@@ -13,7 +13,7 @@ const ContactMe = (props: ContactMeProps) => {
 
     const [content, setContent] = useState('');
     const [email, setEmail] = useState('')
-    const [subject, setSubject]= useState('')
+    const [subject, setSubject] = useState('')
 
 
     const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -26,35 +26,43 @@ const ContactMe = (props: ContactMeProps) => {
         };
 
         fetchData('/api/email', 'POST', emailData)
+
+        alert("form submitted")
+        setEmail('')
+        setSubject('')
+        setContent('')
     };
 
 
     return (
         <Container>
-                    <Card className='m-4 bg-primary' id='blog-box'>
-                        <Card.Body>
-                            <Card.Title className='p-2'>
-                            </Card.Title>
-                            <Form onSubmit={handleFormSubmit}>
-                                <Form.Group className="mb-3 mt-3" controlId="exampleForm.ControlInput1">
-                                    <Form.Label style={{ fontSize: '1.5em' }}>Email:</Form.Label>
-                                    <Form.Control className='bg-light' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    <Form.Label style={{ fontSize: '1.5em' }}>Subject:</Form.Label>
-                                    <Form.Control className='bg-light' type='text' value={subject} onChange={(e) => setSubject(e.target.value)} />
-                                </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <Form.Label style={{ fontSize: '1.5em' }}>Message:</Form.Label>
-                                    <Form.Control
-                                        className='bg-light'
-                                        as="textarea"
-                                        value={content}
-                                        onChange={(e) => setContent(e.target.value)}
-                                        rows={20} />
-                                </Form.Group>
-                                <Button type='submit' variant='secondary'>Submit</Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
+            <Card className='m-4 bg-primary' id='blog-box'>
+                <Card.Body>
+                    <Card.Title style={{ fontSize: '1.8em' }} className='p-2 text-center'>
+                        Contact Me
+                    </Card.Title>
+                    <Form onSubmit={handleFormSubmit}>
+                        <Form.Group className="mb-3 mt-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label style={{ fontSize: '1.5em' }}>Your Email:</Form.Label>
+                            <Form.Control className='bg-light' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label style={{ fontSize: '1.5em' }}>Subject:</Form.Label>
+                            <Form.Control className='bg-light' type='text' value={subject} onChange={(e) => setSubject(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label style={{ fontSize: '1.5em' }}>Message:</Form.Label>
+                            <Form.Control
+                                className='bg-light'
+                                as="textarea"
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                                rows={20} />
+                        </Form.Group>
+                        <Button type='submit' variant='secondary'>Submit</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         </Container>
     );
 };
